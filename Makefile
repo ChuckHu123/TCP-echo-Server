6fb26d9a4d2fcb67b520049a33bfe84644bd438f
ChuckHu123 <hunuochuan@shuyilink.com>
2026-03-26 11:48:28 +0800

    把v1的注释去了，并单独复制了一个包含注释的文件

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 TCP_V1: TCP_V1.c
 	gcc -Wall TCP_V1.c -o TCP_V1
+	gcc -Wall TCP_V1_comment.c -o TCP_V1_comment
 
 helloworld:	hello.c
 	gcc hello.c -o hello
--- TCP_V1_comment.c
+++ TCP_V1_comment.c
@@ -17,7 +17,12 @@ int main() {
     ssize_t recv_len, send_len;
     
     server_fd = socket(AF_INET, SOCK_STREAM, 0); //创建 socket
-
+    /*
+    int socket(int domain, int type, int protocol);
+        domain：即协议域，决定了socket的地址类型AF_INET:IPv4
+        type：指定socket类型。SOCK_STREAM（流式套接字）
+        protocol：就是指定协议，为0时，会自动选择type类型对应的默认协议
+    */
     if (server_fd == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
@@ -28,13 +33,29 @@ int main() {
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
+    /*
+    struct sockaddr_in {
+        sa_family_t    sin_family; / address family: AF_INET 
+        in_port_t      sin_port;   / port in network byte order
+        struct in_addr sin_addr;   / internet address
+        };
 
+    struct in_addr {
+        uint32_t       s_addr;     / address in network byte order 
+        };
+    */
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) { //绑定 socket 到端口
+        // 因为bind /connect/accept 这些系统函数是通用的，所以系统设计了一个通用类型 struct sockaddr，让所有协议都能兼容
         perror("bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
     printf("Bound to port %d.\n", PORT);
+    /*struct sockaddr{  //sockaddr结构体和sockaddr_in结构体都是地址族+端口+IP地址的结构体，可以相互转换
+        sa_family_t  sin_family;   //地址族（Address Family），也就是地址类型
+        char         sa_data[14];  //IP地址和端口号
+        };
+    */
 
     if (listen(server_fd, 1) < 0) { //监听连接，第一个参数为要监听的socket描述字，第二个参数为相应socket可以排队的最大连接个数。
         perror("listen failed");
@@ -48,15 +69,23 @@ int main() {
         if (client_fd < 0) {
             perror("accept failed");
             continue;   // 继续循环，等待下一个连接
+            //close(server_fd);
+            //exit(EXIT_FAILURE);
         }
         printf("Client connected from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        //inet_ntoa()将二进制的IP地址转换为点分十进制的字符串，ntohs()将网络字节序的端口号（大端序）转换为主机字节序的端口号（小端序）
 
         printf("How to disconnect from server: Ctrl+] then type 'quit' and press Enter.\n");
         
         while (1) {
             memset(buffer, 0, BUFFER_SIZE);
             recv_len = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
-
+            /*
+            第一个参数指定发送端套接字描述符；
+            第二个参数指明一个存放应用程序要发送数据的缓冲区；
+            第三个参数指明实际要发送的数据的字节数；留1位存'\0'，避免字符串越界
+            第四个参数一般置0
+            */
             if (recv_len < 0) {
                 perror("recv failed");
                 break;
